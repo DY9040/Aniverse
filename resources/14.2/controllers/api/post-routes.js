b6c4b6151d81bda7afda01297911235f14977795
crocket77 <ethanpfeiffer345@outlook.com>
2022-03-30 23:41:20 -0500

    Revert "removed post url and pushing before i try to reset the database"
    This reverts commit c9cd7dd552d3e736a11a52ebf5d76e79a107592c.

--- resources/14.2/controllers/api/post-routes.js
+++ resources/14.2/controllers/api/post-routes.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
-const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
 
-// get all Posts
+// get all users
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at',[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-    order: [['created_at', 'DESC',]],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        order: [['id', 'ASC',]],//doesnt work?
         include: {
           model: User,
           attributes: ['username']
@@ -35,12 +40,17 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at',[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        order: [['id', 'ASC',]],//doesnt work?
         include: {
           model: User,
           attributes: ['username']
@@ -66,11 +76,11 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {"title": "Taskmaster goes public!", "post_url": "https://taskmaster.com/press", "user_id": "1"}
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id
+    user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -79,15 +89,13 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
-//{"user_id":"1","post_id":"1"}
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
@@ -115,9 +123,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
-
-
 router.delete('/:id', (req, res) => {
+  console.log('id', req.params.id);
   Post.destroy({
     where: {
       id: req.params.id
