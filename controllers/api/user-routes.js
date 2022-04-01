1221c1d42a4ef943bee25bb9fa3c84b2be26f85c
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-01 13:23:18 -0500

    added logout but logout is not working correctly

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote,Comment } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
