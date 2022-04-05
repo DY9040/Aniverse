b8c04c1c4d0ffe53ee8d1723312f9672df0d37e2
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-04 20:07:52 -0500

    Dashboard now displays correct image and started profile pages

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./userpage-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
--- controllers/userpage-routes.js
+++ controllers/userpage-routes.js
@@ -55,52 +55,10 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('dashboard', { user, posts, loggedIn: true });
+      res.render('user', { user, posts, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
-    attributes: [
-      'id',
-      // 'post_url',
-      'title',
-      'created_at',
-      'post_text'
-      // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username','persona','filename']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username','persona','filename']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
-        
-        res.render('edit-post', {
-          post,
-          loggedIn: true
-        });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch(err => {
       res.status(500).json(err);
     });
 });
