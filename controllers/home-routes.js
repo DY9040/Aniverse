e91a8f1b745a240c68e401151d4066ab3f24c258
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-06 16:41:09 -0500

    user page and posts images now display correctly on user.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -76,12 +76,12 @@ router.get('/', (req, res) => {
           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
-            attributes: ['username','persona','filename','user_id']
+            attributes: ['username','persona','filename','id']
           }
         },
         {
           model: User,
-          attributes: ['username','persona','filename','user_id']
+          attributes: ['username','persona','filename','id']
         }
       ]
     })
--- controllers/userpage-routes.js
+++ controllers/userpage-routes.js
@@ -8,13 +8,7 @@ const withAuth = require('../utils/auth');
 router.get('/:id', withAuth, (req, res) => {
   console.log("get from userpage-routes.js worked");
   let user;
-  console.log("req.body:", req.body)
-  console.log('req.params:',req.params);
-  //this is finding user by post id. 
-  User.findOne({
-    where: {
-      id: req.params.id
-    },
+  User.findByPk(req.params.id,{
     attributes: [
       'username',
       'email',
@@ -70,60 +64,5 @@ router.get('/:id', withAuth, (req, res) => {
     });
 });
 
-// router.get('/:id', withAuth, (req, res) => {
-    
-//     let user;
-//     console.log('======================');
-//     User.findByPk(req.session.user_id,{
-//       attributes: [
-//         'username',
-//         'email',
-//         'password',
-//         'persona',
-//         'filename'
-//       ]
-//     }).then(dbUserData => {
-//       user = dbUserData.dataValues;
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-//     Post.findAll({
-//       where: {
-//         user_id: req.session.user_id
-//       },
-//       attributes: [
-//         'id',
-//         'title',
-//         'created_at',
-//         'post_text'
-        
-//       ],
-//       include: [
-//         {
-//           model: Comment,
-//           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//           include: {
-//             model: User,
-//             attributes: ['username','persona','filename']
-//           }
-//         },
-//         {
-//           model: User,
-//           attributes: ['username','persona','filename']
-//         }
-//       ]
-//     })
-//       .then(dbPostData => {
-//         const posts = dbPostData.map(post => post.get({ plain: true }));
-  
-//         res.render('user', { user, posts, loggedIn: true });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
 
 module.exports = router;
